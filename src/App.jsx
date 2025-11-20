785fbda61b8339dc95fc208dcd2deff4a3725ae9
Your Nameabubakarsiddik-1259 <abubakarsiddik1259@gmail.com>
2025-11-20 23:13:54 +0600

    Bannar Dizain Added Compatel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
 import './App.css'
 import Navbar from './componente/Navbar'
+import Bannar from './componente/Page/Bannar.jsx'
 
 function App() {
   
 
   return (
     <>
-    <Navbar></Navbar>
-   
+        <Navbar></Navbar>
+    <div className="max-w-[1176px] mx-auto">
+
+    <Bannar></Bannar>
+   </div>
     </>
   )
 }
---
+++ src/componente/Page/Bannar.jsx
@@ -0,0 +1,42 @@
+
+import React from 'react';
+
+import netImage from "../../assets/vector1.png"
+
+const Bannar = () => {
+    return (
+      
+
+            <div className="mx-8 space-y-22 flex gap-6 my-5">
+
+                <div className="w-full bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-xl p-8 h-45 flex flex-col items-center justify-center flex-1 relative">
+                    <img className='h-45 absolute left-0' src={netImage} alt="" />
+
+                    <img className='h-45 absolute right-0 scale-x-[-1]' src={netImage} alt="" />
+                    <p className='text-base '>In-Progress</p>
+                    <h1 className='text-4xl  font-semibold'>0</h1>
+                </div>
+
+                <div className="w-full bg-gradient-to-r from-[#54CF68] to-[#00827A] text-white rounded-xl relative p-8 h-45 flex flex-col items-center justify-center flex-1">
+                     <img className='h-45 absolute left-0' src={netImage} alt="" />
+                 
+
+                      <img className='h-45 absolute right-0 scale-x-[-1]' src={netImage} alt="" />  
+
+                    <p className='text-base '>Resolved</p>
+                    <h1 className='text-4xl  font-semibold'>0</h1>
+                </div>
+
+
+
+
+
+
+
+            </div>
+            
+       
+    );
+};
+
+export default Bannar;
